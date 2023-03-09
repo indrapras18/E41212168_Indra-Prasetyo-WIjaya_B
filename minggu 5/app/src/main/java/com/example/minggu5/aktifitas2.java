675a776631bdb2606816075d7d04fc8cd8648d32
IndraPras <indrapras2002@gmail.com>
2023-03-09 12:28:32 +0700

    minggu 5

---
+++ minggu 5/app/src/main/java/com/example/minggu5/aktifitas2.java
@@ -0,0 +1,47 @@
+package com.example.minggu5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.os.Bundle;
+
+public class aktifitas2 extends AppCompatActivity {
+    EditText name;
+    Button btnSend;
+    private String KEY_NAME = "NAMA";
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktifitas2);
+        name = (EditText) findViewById(R.id.edt_nama);
+        btnSend = (Button) findViewById(R.id.button_send);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    String nama = name.getText().toString();
+                    if(nama != ""){
+                        Intent i = new Intent(aktifitas2.this, SecondActivity.class);
+                        i.putExtra(KEY_NAME, nama);
+                        startActivity(i);
+
+                    } else {
+                        Toast.makeText(getApplication(), "YOU NEED TO FILL YOUR NAME", Toast.LENGTH_SHORT);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplication(), "ERROR, TRY AGAIN!", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+    }
+}
